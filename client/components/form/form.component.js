b74d129c43a6a2a75e526c1c2c1efc31139d183c
atellmer <atellmer@gmail.com>
2016-11-30 10:29:33 +0500

    form component modified

--- client/components/form/form.component.js
+++ client/components/form/form.component.js
@@ -5,45 +5,20 @@
 		document.addEventListener('DOMContentLoaded', ready);
 
 		function ready() {
-			var uploadTriggerBtns = document.querySelectorAll('[data-upload-trigger]'),
-					uploadTargetBtns = document.querySelectorAll('[data-upload-target]'),
-					hammerUploadTriggerBtns = [],
-					i;
+			var uploadTriggerBtn = document.querySelector('#upload-trigger'),
+					uploadTargetBtn = document.querySelector('#upload-target'),
+					uploadSign = document.querySelector('#upload-sign'),
+					hammerUploadTriggerBtn;
 
-			for (i = 0; i < uploadTriggerBtns.length; i++) {
-				uploadTargetBtns[i].setAttribute('name', 'upload_' + i);
-				hammerUploadTriggerBtns[i] = new Hammer(uploadTriggerBtns[i])
-						.on('tap', uploadBtnHandler.bind(null, uploadTargetBtns[i]));
-			}
+				hammerUploadTriggerBtn = new Hammer(uploadTriggerBtn)
+					.on('tap', uploadBtnHandler);
 
-			function uploadBtnHandler(target) {
-				var parent = target.closest('[data-upload-id]'),
-						parentId = parseInt(parent.getAttribute('data-upload-id')),
-						btn = parent.querySelector('[data-upload-btn]'),
-						sign = parent.querySelector('[data-upload-sign]'),
-						trigger = parent.querySelector('[data-upload-sign-trigger]'),
-						hammerTrigger = new Hammer(trigger),
-						clone;
-
-				target.click();
-				clone = parent.cloneNode(true);
-				clone.setAttribute('data-upload-id', parentId + 1);
-
-				target.addEventListener('change', changeHandler);
-				hammerTrigger.on('tap', clickHandler);
+			function uploadBtnHandler() {
+				uploadTargetBtn.click();
+				uploadTargetBtn.addEventListener('change', changeHandler);
 
 				function changeHandler() {
-					if (!btn.classList.contains('js-is-selected')) {
-						btn.classList.add('js-is-selected');
-					}
-					if (!sign.classList.add('js-is-selected')) {
-						sign.classList.add('js-is-selected');
-					}
-				}
-
-				function clickHandler() {
-					parent.parentNode.appendChild(clone);
-					ready();
+					uploadSign.textContent = 'Выбрано файлов: ' + uploadTargetBtn.files.length;
 				}
 			}
 		}
